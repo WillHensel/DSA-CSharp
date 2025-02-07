a514a45ae7df72f6db13d1e1d423658efc23c6c0
Will Hensel <will@willowispsoftware.com>
2025-02-06 20:49:31 -0800

    Refactored the binary tree traversal tests

--- DSA.Tests/BinaryTreeTests.cs
+++ DSA.Tests/BinaryTreeTests.cs
@@ -3,21 +3,31 @@
 [TestFixture]
 public class BinaryTreeTests
 {
+    public BinaryTree<int> Tree = new (1);
+    
+    //        1                                                             
+    //      /  \                                                             
+    //     2    3                                                           
+    //    / \  / \                                                            
+    //   4  5 6   7                                                           
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        Tree.Root.Left = new BinaryTreeNode<int>(2);
+        Tree.Root.Right = new BinaryTreeNode<int>(3);
+        
+        Tree.Root.Left.Left = new BinaryTreeNode<int>(4);
+        Tree.Root.Left.Right = new BinaryTreeNode<int>(5);
+        
+        Tree.Root.Right.Left = new BinaryTreeNode<int>(6);
+        Tree.Root.Right.Right = new BinaryTreeNode<int>(7);
+    }
 
     [Test]
-    public void TestGetNodes()
+    public void TestGetNodesPreOrder()
     {
-        var tree = new BinaryTree<int>(1);
-        tree.Root.Left = new BinaryTreeNode<int>(2);
-        tree.Root.Right = new BinaryTreeNode<int>(3);
-        
-        tree.Root.Left.Left = new BinaryTreeNode<int>(4);
-        tree.Root.Left.Right = new BinaryTreeNode<int>(5);
-        
-        tree.Root.Right.Left = new BinaryTreeNode<int>(6);
-        tree.Root.Right.Right = new BinaryTreeNode<int>(7);
-
-        var nodes = tree.GetNodesPreOrder();
+        var nodes = Tree.GetNodesPreOrder();
         Assert.That(nodes.Count, Is.EqualTo(7));
 
         var expect = new List<int> {1, 2, 4, 5, 3, 6, 7};
@@ -25,20 +35,28 @@ public class BinaryTreeTests
         {
             Assert.That(nodes[i], Is.EqualTo(expect[i]));
         }
-
-        nodes = tree.GetNodesPostOrder();
+    }
+    
+    [Test]
+    public void TestGetNodesInOrder()
+    {
+        var nodes = Tree.GetNodesInOrder();
         Assert.That(nodes.Count, Is.EqualTo(7));
-
-        expect = new List<int> {4, 5, 2, 6, 7, 3, 1};
+        
+        var expect = new List<int> {4, 2, 5, 1, 6, 3, 7};
         for (int i = 0; i < 7; i++)
         {
             Assert.That(nodes[i], Is.EqualTo(expect[i]));
         }
-        
-        nodes = tree.GetNodesInOrder();
+    }
+    
+    [Test]
+    public void TestGetNodesPostOrder()
+    {
+        var nodes = Tree.GetNodesPostOrder();
         Assert.That(nodes.Count, Is.EqualTo(7));
-        
-        expect = new List<int> {4, 2, 5, 1, 6, 3, 7};
+
+        var expect = new List<int> {4, 5, 2, 6, 7, 3, 1};
         for (int i = 0; i < 7; i++)
         {
             Assert.That(nodes[i], Is.EqualTo(expect[i]));
